e5b7b963d9ebfd7c0c6b47900811ed5854dbaaf3
FBMLJ <lucastavasousa@gmail.com>
2019-07-26 21:08:29 -0300

    requisição funcionando

--- controller/user.js
+++ controller/user.js
@@ -2,11 +2,13 @@ const User = require('../models').user
 // console.log(User)
 module.exports = {
   create(req, res){
-    return User
-    .create({
+    return User.create({
       name: req.body.name,
       password: req.body.password,
-      username: req.body.username
+      username: req.body.username,
+      // updatedAt: Date.now(),
+      // createdAt: Date.now()
+
     })
     .then (user => res.status(201).send(user))
     .catch (error => res.status(400).send(error))
