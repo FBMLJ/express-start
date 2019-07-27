e1cd0b05214bf8d5a633a425f9e19d9d68f2b484
FBMLJ <lucastavasousa@gmail.com>
2019-07-27 09:50:33 -0300

    start to config the cookie sessision

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ const userController = require('../controller').user;
 module.exports = (app) => {
 
   app.post('/cadastre', userController.create);
-  app.get('/cadastre', (req,res) => res.render('user/new'))
+  app.get('/cadastre', (req,res) => res.render('user/sign_up'))
 
   app.get('/',(req,res) => {
     res.render('static_pages/home')
