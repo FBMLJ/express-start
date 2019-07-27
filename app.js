54ad198ba2d720c105f89c7322e564363b030862
FBMLJ <lucastavasousa@gmail.com>
2019-07-26 21:19:06 -0300

    adicionando handlebars

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
+const handlebars = require('express-handlebars')
+
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//config handlebars
+  //Template Engine
+  app.engine('handlebars', handlebars({ defaultLayout: 'main'}))
+  app.set('view engine', 'handlebars')
+  
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
 
