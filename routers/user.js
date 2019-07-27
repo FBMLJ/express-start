cd182d1ce718fde842e307bcc01ea4080912c9ec
FBMLJ <lucastavasousa@gmail.com>
2019-07-27 12:15:09 -0300

    rota do login pronto

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,29 @@
-const userController = require('../controller').user;
+const User = require('../controller').user;
 module.exports = (app, sessionChecker) => {
+
+  //Login
+  app.route('/login')
+    .get(sessionChecker, (req, res) => {
+      res.render('user/sign_in')
+    })
+    .post((req, res) => {
+      var username = req.body.username,
+        password = req.body.password;
+
+      User.findOne({ where: { username: username } }).then(function (user) {
+        if (!user) {
+          res.redirect('/sign_in');
+        } else if (!user.validPassword(password)) {
+          res.redirect('/sign_in');
+        } else {
+          req.session.user = user.dataValues;
+          res.redirect('/home');
+        }
+      });
+    });
+  // app.get('/login', sessionChecker,(req, res) => res.render('user/sign_in'))
   
-  app.get('/login', sessionChecker,(req, res) => res.render('user/sign_in'))
-  
-  app.post('/cadastre', userController.create);
-  app.get('/cadastre', sessionChecker,(req, res) => res.render('user/sign_up'))
+  // app.post('/cadastre', userController.create);
+  // app.get('/cadastre', sessionChecker,(req, res) => res.render('user/sign_up'))
 
 }
