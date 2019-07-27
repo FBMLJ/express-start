32196d736eb68bd3012d39824a2bf96708528f48
FBMLJ <lucastavasousa@gmail.com>
2019-07-27 09:42:25 -0300

    formulario para cadastro

--- models/User.js
+++ models/User.js
@@ -1,10 +1,22 @@
 'use strict';
+var bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
     name: DataTypes.STRING
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    },
+      instanceMethods: {
+        validPassword: function (password) {
+          return bcrypt.compareSync(password, this.password);
+        }
+      }});
   User.associate = function(models) {
     // associations can be defined here
   };
--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,8 @@ const userController = require('../controller').user;
 module.exports = (app) => {
 
   app.post('/cadastre', userController.create);
+  app.get('/cadastre', (req,res) => res.render('user/new'))
+
   app.get('/',(req,res) => {
     res.render('static_pages/home')
   })
